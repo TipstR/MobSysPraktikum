f1e2f0fd55cec4a1b253f90073fed09ab3c39be7
TipstR <96343885+TipstR@users.noreply.github.com>
2025-11-23 13:10:52 +0100

    added logout button and outsourced source code

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword }
+import { getAuth}
     from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, onSnapshot }
+import { getFirestore}
     from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBPsQRyv6E1MS4uOH55Oc6DZ6T0Bm1fXug",
     authDomain: "mob-sys-praktikum.firebaseapp.com",
@@ -21,58 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-document.getElementById("loginBtn").onclick = async () => {
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
-
-    try {
-        await signInWithEmailAndPassword(auth, email, pass);
-        alert("Logged in!");
-    } catch (err) {
-        console.log(err);
-        alert("Login failed: " + err.message);
-    }
-};
-
-document.getElementById("registerBtn").onclick = async () => {
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
-
-    try {
-        await createUserWithEmailAndPassword(auth, email, pass);
-        alert("Registered!");
-    } catch (err) {
-        console.log(err);
-        alert("Registration failed: " + err.message);
-    }
-};
-
-// ---------------- FIRESTORE -------------------
-
-document.getElementById("sendBtn").onclick = async () => {
-    const msg = document.getElementById("msg").value;
-
-    try {
-        await addDoc(collection(db, "messages"), {
-            text: msg,
-            timestamp: Date.now()
-        });
-    } catch (err) {
-        console.log(err);
-        alert("Error writing document: " + err.message);
-    }
-};
-
-// Realtime updates from Firestore
-onSnapshot(collection(db, "messages"), (snapshot) => {
-    const list = document.getElementById("list");
-    list.innerHTML = "";
-
-    snapshot.forEach((doc) => {
-        const data = doc.data();
-        list.innerHTML += `<p>${data.text}</p>`;
-    });
-});
---
+++ scripts/index.js
@@ -0,0 +1,71 @@
+import {auth, db} from "../app.js";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut}
+    from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import {collection, addDoc, onSnapshot}
+    from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+
+// Login
+document.getElementById("loginBtn").onclick = async () => {
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, pass);
+        alert("Logged in!");
+    } catch (err) {
+        console.log(err);
+        alert("Login failed: " + err.message);
+    }
+};
+
+// Logout
+document.getElementById("logoutBtn").onclick = async () => {
+    try {
+        signOut(auth);
+
+        alert("Logged Out!");
+    } catch (err) {
+        console.log(err);
+        alert("Logout failed!");
+    }
+};
+
+// Register
+document.getElementById("registerBtn").onclick = async () => {
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, pass);
+        alert("Registered!");
+    } catch (err) {
+        console.log(err);
+        alert("Registration failed: " + err.message);
+    }
+};
+
+// Send Message
+document.getElementById("sendBtn").onclick = async () => {
+    const msg = document.getElementById("msg").value;
+
+    try {
+        await addDoc(collection(db, "messages"), {
+            text: msg,
+            timestamp: Date.now()
+        });
+    } catch (err) {
+        console.log(err);
+        alert("Error writing document: " + err.message);
+    }
+};
+
+// Read Messages
+onSnapshot(collection(db, "messages"), (snapshot) => {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+        const data = doc.data();
+        list.innerHTML += `<p>${data.text}</p>`;
+    });
+});
