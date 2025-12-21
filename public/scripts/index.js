7e2b7ae4024b9836151eb75119f6f4f6325f2772
TipstR <96343885+TipstR@users.noreply.github.com>
2025-12-21 16:41:08 +0100

    outsource functions

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,73 +1,38 @@
-import {auth, db} from "../app.js";
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut}
-    from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import {collection, addDoc, onSnapshot}
-    from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {login, register, logout, sendMessage} from "../firebaseMethods.js";
 
-console.log("hello world");
+// When Document is loaded:
+document.addEventListener("DOMContentLoaded", () => {
+    // Login
+    document.getElementById("loginBtn").onclick = async () => {
+        const email = document.getElementById("email").value;
+        const pass = document.getElementById("pass").value;
 
-// Login
-document.getElementById("loginBtn").onclick = async () => {
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
-
-    try {
-        await signInWithEmailAndPassword(auth, email, pass);
-        alert("Logged in!");
-    } catch (err) {
-        console.log(err);
-        alert("Login failed: " + err.message);
-    }
-};
+        await login(email, pass);
+    };
 
 // Logout
-document.getElementById("logoutBtn").onclick = async () => {
-    try {
-        signOut(auth);
-
-        alert("Logged Out!");
-    } catch (err) {
-        console.log(err);
-        alert("Logout failed!");
-    }
-};
+    document.getElementById("logoutBtn").onclick = async () => {
+        await logout();
+    };
 
 // Register
-document.getElementById("registerBtn").onclick = async () => {
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
+    document.getElementById("registerBtn").onclick = async () => {
+        const email = document.getElementById("email").value;
+        const pass = document.getElementById("pass").value;
 
-    try {
-        await createUserWithEmailAndPassword(auth, email, pass);
-        alert("Registered!");
-    } catch (err) {
-        console.log(err);
-        alert("Registration failed: " + err.message);
-    }
-};
+        await register(email, pass);
+    };
 
 // Send Message
-document.getElementById("sendBtn").onclick = async () => {
-    const msg = document.getElementById("msg").value;
+    document.getElementById("sendBtn").onclick = async () => {
+        const msg = document.getElementById("msg").value;
 
-    try {
-        await addDoc(collection(db, "messages"), {
-            text: msg,
-            timestamp: Date.now()
-        });
-    } catch (err) {
-        console.log(err);
-        alert("Error writing document: " + err.message);
-    }
-};
-
-// Read Messages
-onSnapshot(collection(db, "messages"), (snapshot) => {
-    const list = document.getElementById("list");
-    list.innerHTML = "";
-
-    snapshot.forEach((doc) => {
-        const data = doc.data();
-        list.innerHTML += `<p>${data.text}</p>`;
-    });
+        await sendMessage(msg);
+    };
 });
+
+
+// Test function for testing the tests :)
+export function helloWorld() {
+    return 'Hello World!';
+}
